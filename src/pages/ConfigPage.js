6befc432b9e9832f4f60034235be675170f1a877
Subodh Jena <jenasubodh@gmail.com>
2022-10-03 20:02:55 +0530

    Save config button

--- src/pages/ConfigPage.js
+++ src/pages/ConfigPage.js
@@ -4,7 +4,7 @@
  * File Created: Monday, 3rd October 2022 1:01:26 pm
  * Author: Subodh Jena (jenasubodh@gmail.com)
  * -----
- * Last Modified: Monday, 3rd October 2022 8:00:03 pm
+ * Last Modified: Monday, 3rd October 2022 8:02:43 pm
  * Modified By: Subodh Jena (jenasubodh@gmail.com>)
  * -----
  * Copyright 2019 - 2022 symbl.ai, Symbl
@@ -13,6 +13,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function ConfigPage(props) {
   const classes = useStyles();
@@ -22,7 +23,7 @@ export default function ConfigPage(props) {
       <div className={classes.mainContainer}>
         <div className={classes.container}>
           <p className={classes.title}>Symbl</p>
-          <form className={classes.containerBody} noValidate>
+          <div className={classes.containerBody} noValidate>
             <TextField id="symbl_url" label="URL" variant="outlined" />
             <span className={classes.separator}></span>
             <TextField id="symbl_app_id" label="App Id" variant="outlined" />
@@ -32,14 +33,14 @@ export default function ConfigPage(props) {
               label="App Secret"
               variant="outlined"
             />
-          </form>
+          </div>
         </div>
 
         <div className={classes.container}>
           <p className={classes.title}>Twilio Bridge</p>
-          <form className={classes.containerBody} noValidate>
+          <div className={classes.containerBody} noValidate>
             <TextField id="twilio_bridge_url" label="URL" variant="outlined" />
-          </form>
+          </div>
         </div>
       </div>
 
@@ -53,6 +54,12 @@ export default function ConfigPage(props) {
           <p className={classes.title}>Trackers</p>
           <div className={classes.containerBody}></div>
         </div>
+      </div>
+
+      <div className={classes.container}>
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
       </div>
     </>
   );
