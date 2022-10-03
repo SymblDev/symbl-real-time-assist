7473292a8c6c50f3d6f59113cef1f01309066aaa
Subodh Jena <jenasubodh@gmail.com>
2022-10-03 20:10:05 +0530

    Removes ids

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 export const apiBase =
   process.env.REACT_APP_SYMBL_CUSTOM_DOMAIN || "https://api-labs.symbl.ai";
-export const symblAppId =
-  "6f365733504b7548367270315550716345614363474a797031794b436e4c5452";
-export const symblAppSecret =
-  "6c55645a5f394b594c394144692d6c436e557732516437615449455f6a3949744c686748764e594d6d71383178346d3036573374645f6f5969325068612d756e";
+export const symblAppId = "";
+export const symblAppSecret = "";
 export const summaryEmails = process.env.REACT_APP_SUMMARY_EMAIL_LIST
   ? process.env.REACT_APP_SUMMARY_EMAIL_LIST.replace(/\s/g, "").split(",")
   : [];
