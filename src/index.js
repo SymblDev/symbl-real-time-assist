cee02f64e8904933fba781d9a354ee9f153be2d8
Subodh Jena <jenasubodh@gmail.com>
2022-10-03 14:49:06 +0530

    Applying theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
 
 import "./index.css";
+import theme from "./Theme";
 import Layout from "./pages/Layout";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout />
+    <ThemeProvider theme={theme}>
+      <Layout />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
